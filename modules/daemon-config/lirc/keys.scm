alezost/guile-daemon-config

commit 051f5e400791d4afb0ec59baa84acc86652af717

lirc/keys: Bind "KEY_RED" to "chvt 12"

modules/daemon-config/lirc/keys.scm
@@ -77,6 +77,8 @@ Return #f if KEY is not a number key."
    ;; Global keys.
    ((key? "KEY_INFO")
     (toggle-clock-osd))
+   ((key? "KEY_RED")
+    (system "sudo chvt 12"))
    ((key? "KEY_CAMERA")
     (system "stumpish -e al/toggle-root < /dev/null"))
    ((key? "KEY_SLEEP")

commit a1cf392814478cf2bea62592cdae30bdc0c174be

lirc/keys: Bind "KEY_RED" to chvt only in "main" mode
This fixes a regression introduced by commit
051f5e400791d4afb0ec59baa84acc86652af717.

It shouldn't be global since "KEY_RED" is used by "tv" mode.

modules/daemon-config/lirc/keys.scm
@@ -77,8 +77,6 @@ Return #f if KEY is not a number key."
    ;; Global keys.
    ((key? "KEY_INFO")
     (toggle-clock-osd))
-   ((key? "KEY_RED")
-    (system "sudo chvt 12"))
    ((key? "KEY_CAMERA")
     (system "stumpish -e al/toggle-root < /dev/null"))
    ((key? "KEY_SLEEP")
@@ -162,6 +160,8 @@ Return #f if KEY is not a number key."
       (call-with-new-thread
        (lambda () (system* "toggle-tvtime")))
       (rc-mode-show "tv"))
+     ((key? "KEY_RED")
+      (system "sudo chvt 12"))
      ((key? "KEY_CHANNELDOWN")
       (set-sound "3%-"))
      ((key? "KEY_CHANNELUP")