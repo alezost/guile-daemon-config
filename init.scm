4e0b489cce7a70476a6b5a05665f9f5a163bdcf2
Alex Kost <alezost@gmail.com>
2026-01-04 09:48:46 +0300

    init: Add modules to '%load-compiled-path'

--- init.scm
+++ init.scm
@@ -1,6 +1,6 @@
 ;;; init.scm --- Configuration file for Guile-Daemon
 
-;; Copyright © 2016 Alex Kost <alezost@gmail.com>
+;; Copyright © 2016–2026 Alex Kost <alezost@gmail.com>
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -26,7 +26,8 @@
 
 (let ((modules (string-append (dirname (current-filename))
                               "/modules")))
-  (push! modules %load-path))
+  (push! modules %load-path)
+  (push! modules %load-compiled-path))
 
 (set-current-module (resolve-module '(daemon-config)))
 
